673639789dbb0979f5324c3faf2f96a549284d0a
Angelic Aether <86170310+Angelic-Aether@users.noreply.github.com>
2024-12-01 13:00:15 +1100

    Initial commit

---
+++ common/src/main/java/net/angelicaether/novaastralis/Constants.java
@@ -0,0 +1,11 @@
+package net.angelicaether.novaastralis;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Constants {
+
+	public static final String MOD_ID = "novaastralis";
+	public static final String MOD_NAME = "Nova Astralis";
+	public static final Logger LOG = LoggerFactory.getLogger(MOD_NAME);
+}
